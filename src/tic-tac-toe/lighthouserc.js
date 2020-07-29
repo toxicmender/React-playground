a2921199622b8f81cc62dda643342653471eed4d
toxicmender <13049649+toxicmender@users.noreply.github.com>
2020-07-29 19:01:41 +0530

    fix: Refactor lighthouse github action to be executed in correct directory feat: Add warn & err assert details in lighthouse report refactor: increase number of runs from default of 3 to 5
    Signed-off-by: toxicmender <13049649+toxicmender@users.noreply.github.com>

--- src/tic-tac-toe/lighthouserc.js
+++ src/tic-tac-toe/lighthouserc.js
@@ -5,6 +5,13 @@ module.exports = {
             // Dynamic site example
             startServerCommand: 'npm run start',
             url: ['http://localhost:3000'],
+            numberOfRuns: 5,
+        },
+        assert: {
+            assertions: {
+                'categories:performance': ['warn', {minScore: 1}],
+                'categories:accessibility': ['error', {minScore: 1}]
+            }
         },
         upload: {
             /* Add configuration here */
